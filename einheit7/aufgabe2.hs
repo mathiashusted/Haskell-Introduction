7f31d048a6fa19bb2ea10a60c00c697cdf33204a
Mathias Husted <lecktmichpls@t-online.de>
2022-12-11 19:45:09 +0100

    Unnötige Hilfsfunktionen durch Prelude ersetzt, Kommentare hinzugefügt

--- einheit7/aufgabe2.hs
+++ einheit7/aufgabe2.hs
@@ -6,65 +6,20 @@
 module Aufgabe2 where
 -- TEILAUFGABE a)
 
--- (Hilfsfunktion)
--- Vor: keine
--- Erg: Liefert Anzahl negativer Werte in x:xs
-low :: [Int] -> Int
-low [] = 0
-low (x:xs)
-  | x < 0 = 1 + low xs -- x ist negativ, addiere 1
-  | x >= 0 = low xs
-  | otherwise = error"Fehler"
-
-
--- Hilfsfunktion
--- Vor: keine
--- Erg: Liefert Anzahl positiver Werte in x:xs
-high :: [Int] -> Int
-high [] = 0
-high (x:xs)
-  | x > 0 = 1 + high xs -- x ist positiv, addiere 1
-  | x <= 0 = high xs
-  | otherwise = error"Fehler"
-
 -- Vor: keine
 -- Erg: Liefert ein Tupel mit der Anzahl positiver Werte in x:xs sowie die Anzahl negativer Werte in x:xs
 lowhigh :: [Int] -> (Int, Int)
-lowhigh [] = (0,0)
-lowhigh (x:xs) = (low (x:xs), high (x:xs)) -- Tupel mit lows und highs von x:xs
-
-
+lowhigh xs = (length (filter (<0) xs), length (filter (>0) xs))
 
 
 -- TEILAUFGABE b)
-
--- HILFSFUNKTION
--- Vor: keine
--- Erg: Liefert Anzahl der Zahlen aus der Liste x:xs, bei denen die Funktion f mit x positiv (> 0) ausgewertet wird
-evaluatesPositive :: (Int -> Int) -> [Int] -> Int
-evaluatesPositive _ [] = 0
-evaluatesPositive f (x:xs)
-  | f x > 0 = 1 + evaluatesPositive f xs -- x ist positiv, addiere 1
-  | f x <= 0 = evaluatesPositive f xs
-  | otherwise = error"Fehler"
-
-
--- HILFSFUNKTION
--- Vor: keine
--- Erg: Liefert Anzahl der Zahlen aus der Liste x:xs, bei denen die Funktion f mit x negativ (< 0) ausgewertet wird
-evaluatesNegative :: (Int -> Int) -> [Int] -> Int
-evaluatesNegative _ [] = 0
-evaluatesNegative f (x:xs)
-  | f x < 0 = 1 + evaluatesNegative f xs -- x ist negativ, addiere 1
-  | f x >= 0 = evaluatesNegative f xs
-  | otherwise = error"Fehler"
 
 -- Vor: keine
 -- Erg: Liefert ein Tupel, die die Anzahl der positiven Auswertungen der Funktion f unter einer beliebigen Liste ausgibt, und für die negativen Auswertungen analog
 lowhighFunc :: (Int -> Int) -> [Int] -> (Int, Int)
 lowhighFunc _ [] = (0,0)
-lowhighFunc f (x:xs) = (evaluatesNegative f (x:xs), evaluatesPositive f (x:xs))
-
+lowhighFunc f xs = (length(filter (<0) (map f xs)),length(filter (>0) (map f xs)))
+-- Gibt ein Tupel aus mit der Liste der Anzahl Male, wo xs jeweils positiv bzw. negativ ausgewertet wurde
 
 -- TEILAUFGABE c)
 
@@ -85,4 +40,4 @@ absmin (x:xs)
 -- Erg: Eine Liste, die alle Elemente der Liste a enthalten, wobei darauf das Prädikat p angewendet wurde und ausschließlich aus booleschen Werten besteht
 toBools :: (a -> Bool) -> [a] -> [Bool]
 toBools _ [] = []
-toBools p xs = map p xs
+toBools p xs = map p xs -- Gebe eine Liste aus, die die Funktion p über map auf jedes Element ausgeführt hat
