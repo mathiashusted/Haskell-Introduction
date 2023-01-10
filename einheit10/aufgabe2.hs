6c2000deecda096da4ff1658faf6712e40a33111
mathiashusted <username@users.noreply.github.com>
2023-01-10 21:32:09 +0100

    Aufgabe a und b

--- einheit10/aufgabe2.hs
+++ einheit10/aufgabe2.hs
@@ -9,4 +9,17 @@ data Menge a = Menge [a]
 
 -- TEILAUFGABE a)
 instance Show a => Show (Menge a) where
-  show (x:xs) = "{" ++ (show x)
+  show (Menge []) = "∅"
+  show (Menge (x:xs)) = "{" ++ show x ++ ", " ++ (show (Menge xs)) ++ "}"
+
+vonMenge :: Eq a => Menge a -> [a]
+vonMenge (Menge []) = []
+vonMenge (Menge (x:xs)) = [x] ++ vonMenge (Menge xs)
+
+vonListe :: Eq a => [a] -> Menge a
+vonListe [] = Menge []
+vonListe [x] = Menge [x]
+vonListe (x:xs)
+  | x `elem` xs = vonListe xs
+  | not(x `elem` xs) = Menge ([x]++(vonMenge(vonListe (xs))))
+
