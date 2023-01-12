d22206633c23134d2d0a5c6568c76eaec22fc5a9
mathiashusted <username@users.noreply.github.com>
2023-01-12 21:06:34 +0100

    Kommentiert, Spezifikationen hinzugefügt

--- einheit10/aufgabe2.hs
+++ einheit10/aufgabe2.hs
@@ -10,17 +10,22 @@ import Data.List
 data Menge a = Menge [a]
 
 -- TEILAUFGABE a)
-instance Show a => Show (Menge a) where
+instance Show a => Show (Menge a) where -- Instanz Show des Datentyps Menge
   show (Menge []) = "∅"
   show (Menge (x:xs)) = "{" ++ (intercalate ", " (map show (x:xs)) ) ++ "}"
 
 
 
 -- TEILAUFGABE b)
+-- Hilfsfunktion
+-- Vor: Keine
+-- Erg: Wandelt Eingegebene Menge in eine interne Liste um
 vonMenge :: Eq a => Menge a -> [a]
 vonMenge (Menge []) = []
 vonMenge (Menge (x:xs)) = [x] ++ vonMenge (Menge xs)
 
+-- Vor: Keine
+-- Erg: Wandelt interne Liste in eine Menge um
 vonListe :: Eq a => [a] -> Menge a
 vonListe [] = Menge []
 vonListe [x] = Menge [x]
@@ -31,7 +36,7 @@ vonListe (x:xs)
 
 
 -- TEILAUFGABE c)
-instance Eq a => Eq (Menge a) where
+instance Eq a => Eq (Menge a) where -- Instanz Eq des Datentyps Menge
   (==) (Menge []) (Menge []) = True
   (==) (Menge (x:xs)) (Menge (y:ys)) = x == y && xs == ys
   (/=) (Menge (x:xs)) (Menge (y:ys)) = x /= y || xs /= ys
@@ -39,19 +44,15 @@ instance Eq a => Eq (Menge a) where
 
 -- TEILAUFGABE d)
 -- Funktion wurde vereinigung statt union genannt, um Konflikte mit Data.List zu vermeiden
+-- Vor: Keine
+-- Erg: Liefert Menge a vereinigt mit Menge b
 vereinigung :: Eq a => Menge a -> Menge a -> Menge a
 vereinigung (Menge []) (Menge []) = (Menge [])
 vereinigung (Menge (x:xs)) (Menge (y:ys)) = (vonListe(x:xs ++ y:ys))
 
+-- Diese Funktion wurde ebenfalls umbenannt
+-- Vor: Keine
+-- Erg: Liefert den Schnitt zwischen Menge a und Menge b
 schnitt :: Eq a => Menge a -> Menge a -> Menge a
 schnitt (Menge []) (Menge []) = (Menge [])
 schnitt (Menge xs) (Menge ys) = vonListe(filter (`elem` ys) xs)
-{-
-schnitt (Menge []) (Menge []) = (Menge [])
-schnitt (Menge []) _ = (Menge [])
-schnitt _ (Menge []) = (Menge [])
-schnitt (Menge (x:xs)) (Menge (y:ys))
-  | x `elem` (y:ys) = vereinigung (Menge [x]) (schnitt (Menge (xs)) (Menge (y:ys)))
-  | not(x `elem` (y:ys)) = schnitt (Menge xs) (Menge (y:ys))
-   -- schnitt (Menge (x:xs)) (Menge (y:ys)) = (Menge (filter (elem (y:ys)) (x:xs)))
--}
